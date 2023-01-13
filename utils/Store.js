893f433d8687408d6bb5925b25a5a2128c9e81ba
ntdat19122002 <ntdat19122002@gmail.com>
2023-01-13 09:10:56 +0700

    Create Shipping Screen

--- utils/Store.js
+++ utils/Store.js
@@ -4,8 +4,8 @@ import Cookies from 'js-cookie';
 export const Store = createContext();
 
 const initialState = {
-    cart: Cookies.get('cart') ? JSON.parse(Cookies.get('cart')):
-    {cartItems:[]},
+    cart: Cookies.get('cart') ? JSON.parse(Cookies.get('cart')) :
+        { cartItems: [], shippingAddress: {} },
 };
 
 function reducer(state, action) {
@@ -20,16 +20,37 @@ function reducer(state, action) {
                     item.name === existItem.name ? newItem : item
                 )
                 : [...state.cart.cartItems, newItem];
-            Cookies.set('cart',JSON.stringify({...state.cart,cartItems}))
+            Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }))
             return { ...state, cart: { ...state.cart, cartItems } };
         }
         case 'CART_REMOVE_ITEM': {
             const cartItems = state.cart.cartItems.filter(
                 (item) => item.slug !== action.payload.slug
             );
-            Cookies.set('cart',JSON.stringify({...state.cart,cartItems}))
+            Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }))
             return { ...state, cart: { ...state.cart, cartItems } };
         }
+        case 'CART_RESET':
+            return {
+                ...state,
+                cart: {
+                    cartItems: [],
+                    shippingAddress: { location: {} },
+                    paymentMethod: '',
+                },
+            };
+        case 'SAVE_SHIPPING_ADDRESS':
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    shippingAddress: {
+                        ...state.cart.shippingAddress,
+                        ...action.payload,
+                    },
+                },
+            };
+
         default:
             return state;
     }
